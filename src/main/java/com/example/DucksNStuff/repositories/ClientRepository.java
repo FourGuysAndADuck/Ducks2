ecc89a47468ec622505d85112fcd0f28b39590f9
Zachary Johnson <zachary.a.johnson42@gmail.com>
2022-10-05 11:58:50 -0500

    added user functionality. tables have been created, endpoint for functionality is in UsersController.

--- src/main/java/com/example/DucksNStuff/repositories/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.DucksNStuff.repositories;
-
-import com.example.DucksNStuff.data.Client;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClientRepository extends JpaRepository<Client, Long> {
-}
---
+++ src/main/java/com/example/DucksNStuff/repositories/UsersRepository.java
@@ -0,0 +1,7 @@
+package com.example.DucksNStuff.repositories;
+
+import com.example.DucksNStuff.data.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UsersRepository extends JpaRepository<Users, Long> {
+}
