7021019c5ce6d76b96cd27e8ac82bc5a1523a1d7
Darien Salazar <darien.axell.salazar@gmail.com>
2022-10-06 16:33:17 -0500

    Adjusted the routing and navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,36 @@
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
 import NavbarComponent from "./components/Navbar"
 import Footer from "./components/Footer";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import Help from "./components/Help";
+import Checkout from "./components/Checkout";
+import NotFound from "./components/NotFound";
+
 import '@coreui/coreui/dist/css/coreui.min.css'
 
-function App(props) {
-    // const {clients} = this.state;
-
+function App() {
       return (
-          <>
-          <div className="App">
-              <NavbarComponent/>
-              <Footer/>
-          </div>
-          </>
+          <Router>
+              <div className="App">
+                  <NavbarComponent/>
+                  <div className="content">
+                      <Routes>
+                          <Route path="/" element={<Home/>}/>
+                          <Route path="/contact" element={<Contact/>}/>
+                          <Route path="/login" element={<Login/>}/>
+                          <Route path="/signup" element={<SignUp/>}/>
+                          <Route path="/help" element={<Help/>}/>
+                          <Route path="/checkout" element={<Checkout/>}/>
+                          <Route path="*" element={<NotFound/>}/>
+                      </Routes>
+                  </div>
+                  <Footer/>
+              </div>
+          </Router>
       );
 }
 
