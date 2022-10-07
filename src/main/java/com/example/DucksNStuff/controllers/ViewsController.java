c8dd4926348e6402779630d67beab0a80759cae3
Zachary Johnson <zachary.a.johnson42@gmail.com>
2022-10-07 09:24:38 -0500

    merging main branch differences

---
+++ src/main/java/com/example/DucksNStuff/controllers/ViewsController.java
@@ -0,0 +1,5 @@
+package com.example.DucksNStuff.controllers;
+
+public class ViewsController {
+
+}
---
+++ src/main/java/com/example/DucksNStuff/data/Orders.java
@@ -0,0 +1,14 @@
+package com.example.DucksNStuff.data;
+
+public class Orders {
+
+    private long id;
+
+    private String status;
+
+    private long quantity;
+
+    //Join Table with Product id, user_id, and order id?
+
+
+}
