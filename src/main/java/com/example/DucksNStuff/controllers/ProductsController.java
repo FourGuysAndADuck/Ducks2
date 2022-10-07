d95c2f39d580d387e9cbd8e8862ce25fb5234918
Zachary Johnson <zachary.a.johnson42@gmail.com>
2022-10-07 10:19:34 -0500

    Added SCRUD functionality to Stores and Products. Still need to work on a few get methods.

--- src/main/java/com/example/DucksNStuff/controllers/ProductsController.java
+++ src/main/java/com/example/DucksNStuff/controllers/ProductsController.java
@@ -1,23 +1,66 @@
 package com.example.DucksNStuff.controllers;
 
+import com.example.DucksNStuff.data.Products;
+import com.example.DucksNStuff.data.Users;
 import com.example.DucksNStuff.repositories.ProductsRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("//////")
+@RequestMapping("/products")
 public class ProductsController {
     //Repository Injection
     ProductsRepository productsRepository;
 
-    //TODO:Get a product
+    //TODO: Get all products by StoreID?
 
-    //TODO:Create a new product
+    //DONE:Get a product by ID
+    @CrossOrigin
+    @GetMapping("/{id}")
+    public Optional<Products> fetchProductById(@PathVariable long id){
+        return productsRepository.findById(id);
+    }
+
+    //TODO:Get products by category. Should this be here or CategoryController?
+
+    //DONE:Create a new product
+    @CrossOrigin
+    @PostMapping("/{id}")
+    public ResponseEntity createUser(@RequestBody Products newProduct) throws URISyntaxException {
+        Products createdProduct = productsRepository.save(newProduct);
+        //return 201
+        return ResponseEntity.created(new URI("/products/" + createdProduct.getId())).body(createdProduct);
+    }
 
     //TODO:Update new Product
+    @CrossOrigin
+    @PutMapping("/{id}")
+    public ResponseEntity updateUser(@RequestBody Products updatedProduct, @PathVariable long id){
+        //Get product by id
+        Optional<Products> originalProductOptional = productsRepository.findById(id);
+        //unwrap
+        Products originalProduct = originalProductOptional.get();
+        //BeanUtils copy
+        BeanUtils.copyProperties(updatedProduct, originalProduct);
+        //Save
+        productsRepository.save(originalProduct);
+        //200 response
+        return ResponseEntity.ok(originalProduct);
+    }
 
     //TODO:Delete a product
+    @CrossOrigin
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteUser(@PathVariable long id){
+        productsRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
 
 }
--- src/main/java/com/example/DucksNStuff/controllers/StoresController.java
+++ src/main/java/com/example/DucksNStuff/controllers/StoresController.java
@@ -1,22 +1,64 @@
 package com.example.DucksNStuff.controllers;
 
+import com.example.DucksNStuff.data.Stores;
+import com.example.DucksNStuff.data.Users;
 import com.example.DucksNStuff.repositories.StoresRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Optional;
 
 @AllArgsConstructor
 @RestController
-@RequestMapping("////")
+@RequestMapping("/store")
 public class StoresController {
     //Injection
     StoresRepository storesRepository;
+
     //TODO:Get Store info
+    @CrossOrigin
+    @GetMapping("/{id}")
+    public Optional<Stores> fetchStoreById(@PathVariable long id){
+       return storesRepository.findById(id);
+    }
+
 
     //TODO:Create New Store
-
+    @CrossOrigin
+    @GetMapping("/create")
+    public ResponseEntity createStore(@RequestBody Stores newStore) throws URISyntaxException {
+        Stores createdStore = storesRepository.save(newStore);
+        //return 201
+        return ResponseEntity.created(new URI("/store/" + createdStore.getId())).body(createdStore);
+    }
     //TODO:Update Store
+    @CrossOrigin
+    @PutMapping("/{id}")
+    public ResponseEntity updateStore(@PathVariable long id, @RequestBody Stores updatedStore){
+        //Getting store by ID
+        Optional<Stores> originalStoreOptional = storesRepository.findById(id);
+        //Unwrapping from Optional
+        Stores originalStore = originalStoreOptional.get();
+
+        //BeanUtils to update only fields that have changed to the original
+            BeanUtils.copyProperties(updatedStore, originalStore);
+        //Saving changes to database
+        storesRepository.save(originalStore);
+        //Returning 200
+        return ResponseEntity.ok(originalStore);
+        }
 
     //TODO:Delete Store
+    @CrossOrigin
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteStore(@PathVariable long id){
+        storesRepository.deleteById(id);
+        //200
+        return ResponseEntity.ok().build();
+    }
 
 }
