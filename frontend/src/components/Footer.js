70788e65773a3abfe9d26fa1c02c93979439a610
midlynj <jmidlyn@gmail.com>
2022-10-19 21:29:33 -0500

    footer fixed to be stationary

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,11 +1,12 @@
 import {CFooter, CLink} from "@coreui/react";
 import {Link} from "react-router-dom";
+import "../footer.css"
 
 const Footer =  () => {
     return(
         <>
         <CFooter>
-            <div>
+            <div className="footer">
                 <CLink to="https://www.google.com">Google</CLink>
                 <span>&copy; 2022 DuckyProductions</span>
             </div>
