ac112dcca766750809e45e8e905c23ec1c6ebe02
midlynj <jmidlyn@gmail.com>
2022-10-24 15:10:27 -0500

    carousel reflect products

---
+++ frontend/src/PexelImage.js
@@ -0,0 +1,17 @@
+import i1 from "../src/ducks/i1.jpeg"
+import i2 from "../src/ducks/i2.jpeg"
+import i3 from "../src/ducks/i3.jpeg"
+import i4 from "../src/ducks/i4.jpeg"
+import i5 from "../src/ducks/i5.jpeg"
+import i6 from "../src/ducks/i6.jpeg"
+import i7 from "../src/ducks/i7.jpeg"
+import i8 from "../src/ducks/i8.jpeg"
+import i9 from "../src/ducks/i9.jpeg"
+import i10 from "../src/ducks/i10.jpeg"
+import i11 from "../src/ducks/i11.jpeg"
+import i12 from "../src/ducks/i12.jpeg"
+import i13 from "../src/ducks/i13.jpeg"
+
+
+
+export default [i1,i2,i3,i4,i5,i6,i7,i8,i9,i10,i11,i12,i13]
